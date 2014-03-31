cd69883b860f2c009fae7d3dc38286b49ae0acd5
alexreedhill <ahill1994@student.elgin.edu>
2014-03-31 09:47:45 -0500

    Refactor ServerRunner

--- src/ServerRunner.java
+++ src/ServerRunner.java
@@ -1,21 +1,30 @@
+import java.io.IOException;
+
 public class ServerRunner {
 
     public static void main(String[] args)  {
         try {
-            int port = Integer.parseInt(args[0]);
-            String publicPath = args[1];
-            Server server = new Server(port, publicPath);
-            new Thread(server).start();
-            try {
-                Thread.sleep(10 * 10000000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            startServer(args);
+            sleep();
             System.out.println("Stopping Server");
         } catch(Exception ex) {
             System.err.println(ex);
         }
+    }
 
+    private static void startServer(String[] args) throws IOException {
+        int port = Integer.parseInt(args[0]);
+        String publicPath = args[1];
+        Server server = new Server(port, publicPath);
+        new Thread(server).start();
+    }
+
+    private static void sleep() {
+        try {
+            Thread.sleep(10 * 10000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
 
